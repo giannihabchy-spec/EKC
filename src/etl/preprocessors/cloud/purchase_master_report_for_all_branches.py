9c45fbe87176e7413a59dfb888672df71cd19df4
Gianni <giannihabchy@gmail.com>
2026-05-27 18:15:42 +0300

    omega name / cloud

--- src/etl/preprocessors/cloud/purchase_master_report_for_all_branches.py
+++ src/etl/preprocessors/cloud/purchase_master_report_for_all_branches.py
@@ -1,15 +1,19 @@
 # All Branches
 
-import pandas as pd
 from etl.utils import read
 from etl.utils import keep_cols_by_index
 from etl.utils import drop_na_by_name
 from etl.utils import remove_repeated_headers
 from etl.utils import make_columns_date, make_columns_numeric
+from etl.utils import get_omega_client_name
 
 
-def preprocess(path):
+def preprocess(path, omega_loc: bool = False):
     data = read(path)
+
+    if omega_loc:
+        omega_client = get_omega_client_name(data)
+
     data = keep_cols_by_index(data,[1,4,5,6,7,9,13])
     data.columns = ['Location','Supplier','Purchase Date','Invoice','Product Description','Qty','Total']
     cols = ['Location','Product Description','Qty','Total','Supplier','Invoice','Purchase Date']
@@ -19,4 +23,10 @@ def preprocess(path):
     data = make_columns_numeric(data,['Total','Qty'])
     data = make_columns_date(data,['Purchase Date'])
     data.columns = ['location', 'raw materials','qty','total cost','supplier names','invoice #','purchase date']
+
+    if omega_loc:
+        data['omega client name'] = omega_client
+        cols = ['omega client name', 'raw materials', 'qty']
+        data = data[cols]
+
     return data
--- src/etl/preprocessors/cloud/sales_by_items.py
+++ src/etl/preprocessors/cloud/sales_by_items.py
@@ -1,16 +1,26 @@
-import pandas as pd
 from etl.utils import read
 from etl.utils import keep_cols_by_index
 from etl.utils import drop_na_by_name
 from etl.utils import remove_repeated_headers
 from etl.utils import make_columns_numeric
+from etl.utils import get_omega_client_name
 
 
-def preprocess(path):
+def preprocess(path, omega_loc: bool = False):
     data = read(path)
+
+    if omega_loc:
+        omega_client = get_omega_client_name(data)
+
     data = keep_cols_by_index(data,[0,2,3])
     data.columns = ['description', 'qty sold', 'gross sales']
     data = drop_na_by_name(data,['qty sold'])
     data = remove_repeated_headers(data,'description')
     data = make_columns_numeric(data,['qty sold','gross sales'])
+
+    if omega_loc:
+        data['omega client name'] = omega_client
+        cols = ['omega client name', 'description', 'qty sold']
+        data = data[cols]
+
     return data

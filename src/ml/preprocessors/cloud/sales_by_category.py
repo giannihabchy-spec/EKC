4b59b5065d451db82ac49b175da4682c8007765b
Gianni <giannihabchy@gmail.com>
2026-06-01 11:11:35 +0300

    preprocessors / use originals

--- src/ml/preprocessors/cloud/sales_by_category.py
+++ src/ml/preprocessors/cloud/sales_by_category.py
@@ -1,32 +1,8 @@
-import pandas as pd
-from etl.utils import read
-from etl.utils import keep_cols_by_index
-from etl.utils import drop_na_by_name
-from etl.utils import drop_rows
-from etl.utils import make_columns_numeric
-from etl.utils import get_omega_client_name
-from etl.utils import get_file_date
-from etl.utils import make_columns_date
+from etl.preprocessors.cloud.sales_by_category import preprocess as original_prep
+from supa.modeling import normalize_column_name
 
 
 def preprocess(path):
-    data = read(path)
-    dt = get_file_date(data, (2,2))
-    omega_name = get_omega_client_name(data)
-    total_col = int(data.iloc[3].last_valid_index())
-    data = keep_cols_by_index(data,[0,total_col])
-    data.columns = ['category', 'sales']
-    data = drop_na_by_name(data,['category','sales'])
-    data = drop_rows(data,'category',value = 'Total ')
-    data = make_columns_numeric(data,['sales'])
-    sum_row = pd.DataFrame({
-        'category': ['calculated_sum'],
-        'sales': [data.sales.sum()]
-    })
-    data = pd.concat([data, sum_row], ignore_index=True)
-    data['omega_name'] = omega_name
-    data['report_date'] = dt
-    data = make_columns_date(data, ['report_date'])
-    cols = ['omega_name', 'report_date', 'category', 'sales']
-    data = data[cols]
+    data = original_prep(path, omega_loc=True)
+    data.columns = [normalize_column_name(col) for col in data.columns]
     return data
--- src/ml/preprocessors/local/sales_by_category.py
+++ src/ml/preprocessors/local/sales_by_category.py
@@ -1,36 +1,10 @@
-import pandas as pd
-from etl.utils import read
-from etl.utils import drop_na_by_name
-from etl.utils import make_columns_numeric
-from etl.utils import make_columns_date
-from etl.utils import get_file_date
-from etl.utils import get_omega_client_name
+from etl.preprocessors.local.sales_by_category import preprocess as original_prep
+from supa.modeling import normalize_column_name
 
 def preprocess(path):
-    data = read(path)
-    dt = get_file_date(data, (9,7), 'local')
-    omega_name = get_omega_client_name(data, (1,0))
-    col_idx = 2
-    first_row = data.iloc[:, col_idx].first_valid_index()
-    last_row = data.iloc[:, col_idx].last_valid_index()
-    row_idx = 12
-    valid_cols = data.iloc[row_idx].notna()
-    first_col = valid_cols.idxmax()
-    last_col = valid_cols[::-1].idxmax()
-    data = data.iloc[first_row : last_row + 1, first_col : last_col + 1].copy()
-    data = data.iloc[:,[0,-1]].copy()
-    data.columns = ['category', 'sales']
-    data['sales'] = data['sales'].shift(-1)
-    data = drop_na_by_name(data, ['category'])
-    data = make_columns_numeric(data, ['sales'])
-    sum_row = pd.DataFrame({
-        'category': ['calculated_sum'],
-        'sales': [data.sales.sum()]
-    })
-    data = pd.concat([data, sum_row], ignore_index=True)
-    data['omega_name'] = omega_name
-    data['report_date'] = dt
-    data = make_columns_date(data, ['report_date'])
-    cols = ['omega_name', 'report_date', 'category', 'sales']
+    data = original_prep(path, omega_loc=True)
+    data.columns = [normalize_column_name(col) for col in data.columns]
+    data = data.rename(columns = {'file_date': 'report_date'})
+    cols = ['category', 'sales', 'omega_name', 'report_date']
     data = data[cols]
     return data

ac6a2e263b45bf449bd96729dfc4fcfc0ab7eba0
Gianni <giannihabchy@gmail.com>
2026-06-15 17:46:43 +0300

    ml / evaluator

---
+++ src/ml/functions/eval.py
@@ -0,0 +1,21 @@
+from sklearn.metrics import (
+    mean_absolute_error,
+    mean_squared_error,
+    mean_absolute_percentage_error
+)
+import numpy as np
+
+
+def evaluate_model(model, val):
+    forecast = model.predict(n_periods=len(val))
+
+    mae = mean_absolute_error(val, forecast)
+    rmse = np.sqrt(mean_squared_error(val, forecast))
+    mape = mean_absolute_percentage_error(val, forecast)
+
+    return {
+        "forecast": forecast,
+        "MAE": mae,
+        "RMSE": rmse,
+        "MAPE": mape
+    }

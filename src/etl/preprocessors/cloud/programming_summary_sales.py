69c132a29531d6d4b908ba2cb642668761fdea2e
Gianni <giannihabchy@gmail.com>
2026-08-06 13:18:32 +0300

    etl / prog sum sales

--- src/etl/preprocessors/cloud/programming_summary_sales.py
+++ src/etl/preprocessors/cloud/programming_summary_sales.py
@@ -12,7 +12,7 @@ def preprocess(path):
     data = read(path)
     data = keep_cols_by_index(data, [0,1,5])
     data.columns = ['group', 'menu items', 'sp exc vat']
-    # data['id'] = data['group']
+    data['id'] = data['group']
     ids = data.loc[data['group'] == 'ID'].index
     remove_ids = ids.union(ids-1).union(ids-2).union(ids-3)
     data = data.drop(index= remove_ids).reset_index(drop=True)
@@ -26,7 +26,7 @@ def preprocess(path):
     data[['group', 'category']] = data[['group', 'category']].ffill()
     data = drop_na_by_name(data, ['menu items'])
     cols = ['category', 'group', 'menu items', 'sp exc vat']
-    # cols = ['id', 'category', 'group', 'menu items', 'sp exc vat']
+    cols = ['id', 'category', 'group', 'menu items', 'sp exc vat']
     data = data[cols]
     data = make_columns_numeric(data, ['sp exc vat'])
     return data

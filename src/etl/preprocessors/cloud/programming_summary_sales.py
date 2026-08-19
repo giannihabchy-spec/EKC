2e97226cfcf8ff2bc6dd8bccb61db57758d3ec29
Gianni <giannihabchy@gmail.com>
2026-08-19 12:14:41 +0300

    etl / prog sum sales

--- src/etl/preprocessors/cloud/programming_summary_sales.py
+++ src/etl/preprocessors/cloud/programming_summary_sales.py
@@ -4,29 +4,34 @@ from etl.utils import (
     read,
     keep_cols_by_index,
     drop_na_by_name,
-    make_columns_numeric
+    make_columns_numeric,
+    drop_rows
 )
 
 
 def preprocess(path):
     data = read(path)
-    data = keep_cols_by_index(data, [0,1,5])
+    data = keep_cols_by_index(data, [0,1,4])
     data.columns = ['group', 'menu items', 'sp exc vat']
-    data['id'] = data['group']
-    ids = data.loc[data['group'] == 'ID'].index
+    ids = data.loc[data['group'] == 'Item ID'].index
     remove_ids = ids.union(ids-1).union(ids-2).union(ids-3)
     data = data.drop(index= remove_ids).reset_index(drop=True)
+    data = drop_rows(data, 'menu items', value = 'Description')
+    data = drop_rows(data, 'group', date = True)
+    data = data.reset_index(drop = True)
+    first_id = data[data['menu items'].notna()].index[0]
+    data.iloc[first_id:, 0] = data.iloc[first_id:, 0].shift(-1)
+    data['id'] = data['group']
     mask = (
         data['menu items'].isna() &
         data['menu items'].shift(-1).isna() & 
         data['menu items'].shift(-2).isna()
     )
     data.loc[mask, 'category'] = data.loc[mask, 'group']
-    data.loc[data['menu items'].notna(), ['group']] = np.nan
-    data[['group', 'category']] = data[['group', 'category']].ffill()
+    data.loc[data['menu items'].notna(), 'group'] = np.nan
+    data[['category', 'group']] = data[['category', 'group']].ffill()
     data = drop_na_by_name(data, ['menu items'])
-    cols = ['category', 'group', 'menu items', 'sp exc vat']
-    cols = ['id', 'category', 'group', 'menu items', 'sp exc vat']
-    data = data[cols]
     data = make_columns_numeric(data, ['sp exc vat'])
+    cols = ['id', 'category', 'group', 'menu items', 'sp exc vat']
+    data = data[cols].copy()
     return data
